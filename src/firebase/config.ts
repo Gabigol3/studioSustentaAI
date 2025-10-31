4dc80514d16208515856c595eb2514759cc0e734
Gabriel Pedro <gabrielpedroramos26@gmail.com>
2025-10-31 17:14:15 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-56495598-6bbc4",
+  "appId": "1:549333416841:web:51789df995a5bac4ef7f58",
+  "apiKey": "AIzaSyCuVn-HdsJwhLw99Q8SY09o7IaYk9LK2GI",
+  "authDomain": "studio-56495598-6bbc4.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "549333416841"
+};
