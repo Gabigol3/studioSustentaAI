bb3ec19730b56c5665686804c115d6d8e895b1b2
Gabriel Pedro Ramos <116090948+Gabigol3@users.noreply.github.com>
2026-04-09 20:31:14 -0300

    Update config.ts

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,9 @@
 export const firebaseConfig = {
-  "projectId": "studio-56495598-6bbc4",
-  "appId": "1:549333416841:web:51789df995a5bac4ef7f58",
-  "apiKey": "AIzaSyCuVn-HdsJwhLw99Q8SY09o7IaYk9LK2GI",
-  "authDomain": "studio-56495598-6bbc4.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "549333416841"
+  apiKey: "AIzaSyAz2fAzowCIkOfebT9pSCY96sqwShxNoPs",
+  authDomain: "studiosustentaai.firebaseapp.com",
+  projectId: "studiosustentaai",
+  storageBucket: "studiosustentaai.firebasestorage.app",
+  messagingSenderId: "543589609335",
+  appId: "1:543589609335:web:cca4e72c93660291effb72",
+  measurementId: "G-8VV8J8DVQE"
 };
